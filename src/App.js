b610b858d3c7a9076060d9fab9cb25b5001d5fcd
abdoulrhman <abdoulrhman_salah@hotmail.com>
2020-02-28 15:13:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import Wrapper from "../src/containers/Wrapper/wrapper";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Wrapper />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Clock/clock.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./clock.css";
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString()
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString()
+    });
+  }
+  render() {
+    return <h1> {this.state.time}</h1>;
+  }
+}
+
+export default Clock;
